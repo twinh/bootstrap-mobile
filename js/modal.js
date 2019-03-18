a1376aaaa0117360c00d437c6b1f23223f002a07
twinh <twinhuang@qq.com>
2019-03-18 23:26:15 +0800

    refactoring: 更改modal实现不超出屏幕

--- js/modal.js
+++ js/modal.js
@@ -1,38 +1,28 @@
 (function ($) {
-    // 对底部modal框超出屏幕高度做自适应
-    var oldShow = $.fn.modal.Constructor.prototype.show;
-    $.fn.modal.Constructor.prototype.show = function () {
-        oldShow.call(this, arguments);
-        this.updateBodyHeight($(this._element));
-    };
+  // 解决iOS底部弹窗后可以继续往下滚动的问题
+  // https://github.com/twbs/bootstrap/issues/14839
+  var scrollTop = 0;
+  var $doc = $(document);
+  var $root = $('html, body');
+  var $body = $('body');
+  $doc.on('show.bs.modal', '.modal-bottom', function (e) {
+    scrollTop = $body.scrollTop();
+    $root.addClass('modal-fixfixed');
+  });
 
-    var oldHandleUpdate = $.fn.modal.Constructor.prototype.handleUpdate;
-    $.fn.modal.Constructor.prototype.handleUpdate = function () {
-        oldHandleUpdate.call(this, arguments);
-        this.updateBodyHeight($(this._element));
-    };
+  $doc.on('hide.bs.modal', '.modal-bottom', function () {
+    $root.removeClass('modal-fixfixed');
+    $body.scrollTop(scrollTop);
+  });
 
-    $.fn.modal.Constructor.prototype.updateBodyHeight = function ($element) {
-        if (!$element.hasClass('modal-bottom')) {
-            return;
-        }
-        var height = $element.find('.modal-header').outerHeight() + $element.find('.modal-footer').outerHeight() - 1;
-        $element.find('.modal-body').css('max-height', $(window).height() - height);
-    };
-
-    // 解决iOS底部弹窗后可以继续往下滚动的问题
-    // https://github.com/twbs/bootstrap/issues/14839
-    var scrollTop = 0;
-    var $doc = $(document);
-    var $root = $('html, body');
-    var $body = $('body');
-    $doc.on('show.bs.modal', '.modal-bottom', function() {
-        scrollTop = $body.scrollTop();
-        $root.addClass('modal-fixfixed');
-    });
-
-    $doc.on('hide.bs.modal', '.modal-bottom', function() {
-        $root.removeClass('modal-fixfixed');
-        $body.scrollTop(scrollTop);
-    });
+  // 确保高度不超出屏幕
+  $doc.on('shown.bs.modal', '.modal-bottom', function (e) {
+    var $element = $(e.target);
+    $element.find('.modal-body').css(
+      'max-height',
+      $(window).height()
+      - $element.find('.modal-header').outerHeight()
+      - $element.find('.modal-footer').outerHeight()
+    );
+  });
 })(jQuery);
